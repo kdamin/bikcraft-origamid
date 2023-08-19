229ef22869695ab159d7f7e8a300198109c5fce2
Kelyn Damin <kelydamin@gmail.com>
2023-08-19 11:30:07 -0300

    Implementação do script para Perguntas Frequentes

--- js/script.js
+++ js/script.js
@@ -1,31 +1,46 @@
-
 // Ativar Links do Menu
-const links = document.querySelectorAll(".header-menu a");
+const links = document.querySelectorAll('.header-menu a')
 
-  function ativarLink(link) {
-    const url = location.href;
-    const href = link.href;
-    if (url.includes(href)) {
-      link.classList.add("ativo");
-    }
+function ativarLink(link) {
+  const url = location.href
+  const href = link.href
+  if (url.includes(href)) {
+    link.classList.add('ativo')
   }
+}
 
-  links.forEach(ativarLink);
+links.forEach(ativarLink)
 
-  // Ativar Itens do Orçamento
+// Ativar Itens do Orçamento
 
-  const parametros = new URLSearchParams(location.search);
+const parametros = new URLSearchParams(location.search)
 
-  function ativarProduto(parametro) {
-    const elemento = document.getElementById(parametro);
-    if(elemento) {
-      elemento.checked = true;
-    }
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro)
+  if (elemento) {
+    elemento.checked = true
   }
+}
 
-  parametros.forEach(ativarProduto);
+parametros.forEach(ativarProduto)
 
+//Perguntas Frequentes
 
+const perguntas = document.querySelectorAll('.perguntas button')
 
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
 
+ 
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
 
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
